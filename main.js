8654d499f563a8d5e9a042a60e033b6d179f445e
luanafront <luana.lopes.rodrigues1@gmail.com>
2023-11-29 17:52:38 -0300

    feat: inclementation of for instead of while

--- main.js
+++ main.js
@@ -3,9 +3,8 @@ function tocaSom(idElementoAudio) {
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-let contador = 0;
 
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador ++) {
   const tecla = listaDeTeclas[contador]
   const instrumento = tecla.classList[1];
   const idAudio = `#som_${instrumento}`;
@@ -13,6 +12,4 @@ while (contador < listaDeTeclas.length) {
   tecla.onclick = function () {
     tocaSom(idAudio);
   }
-
-  contador = contador + 1;
 }
